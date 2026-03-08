a821d357d62a3d4cea2f62fc3168a9cd92ff3e1e
ahanafintesher <ahanafintesher01@gmail.com>
2026-03-08 12:51:16 +0600

    closed functionallity added

--- script/home.js
+++ script/home.js
@@ -1,5 +1,7 @@
 let allData = [];
 
+// data fetching
+
 const loadAllData = () => {
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((res) => res.json())
@@ -11,6 +13,8 @@ const loadAllData = () => {
 
 let issueCount = document.getElementById("issue-count");
 const allSection = document.getElementById("all-section");
+
+// rendering all data
 
 const renderAllData = (infos) => {      
   allSection.innerHTML = '';            
@@ -127,6 +131,8 @@ const renderAllData = (infos) => {
   }
 };
 
+// button functionallity
+
 let allFilterBtn = document.getElementById('all-btn');
 let openFilterBtn = document.getElementById('open-btn');
 let closeFilterBtn = document.getElementById('close-btn');
@@ -146,6 +152,19 @@ openFilterBtn.addEventListener('click', function(){
     openFilterBtn.classList.add('btn-primary');
     renderOpenData();
 })
+closeFilterBtn.addEventListener('click', function(){
+  allFilterBtn.classList.remove('btn-primary');
+  allFilterBtn.classList.add('btn-base');
+  openFilterBtn.classList.remove('btn-primary');
+  openFilterBtn.classList.add('btn-base');
+  closeFilterBtn.classList.remove('btn-base');
+  closeFilterBtn.classList.add('btn-primary');
+
+  renderCloseData();
+})
+
+
+// rendering open data
 
 const renderOpenData = () => {                                              
     let openIssue = allData.filter(issue => issue.status === "open");
@@ -207,5 +226,72 @@ const renderOpenData = () => {
         allSection.appendChild(div);
     }
 }
+
+// rendering closed data
+
+const renderCloseData = () => {                                              
+    let closeIssue = allData.filter(issue => issue.status === "closed");
+    issueCount.innerText = closeIssue.length;
+    allSection.innerHTML = '';         
+    for(let issue of closeIssue){  
+        let div = document.createElement('div');     
+        div.innerHTML =
+        `
+        <div class="bg-white rounded-2xl overflow-hidden h-80 w-80 shadow-sm border border-gray-100">
+  
+  <!-- Top Green Bar -->
+  <div class="h-1 bg-[#A855F7] w-full"></div>
+
+  <div class="p-5">
+    <!-- Top Row: Icon Image + Priority Badge -->
+    <div class="flex justify-between items-center mb-4">
+      <!-- Icon Image -->
+      <img src="./assets/Closed- Status .png" alt="status icon" class="w-9 h-9" />
+
+      <!-- Priority Badge -->
+      <span class="bg-red-100 text-red-400 text-xs font-bold px-3 py-1 rounded-full tracking-widest">
+        ${issue.priority}
+      </span>
+    </div>
+
+    <!-- Title -->
+    <h2 class="text-gray-900 font-bold text-base leading-snug mb-2">
+     ${issue.title}
+    </h2>
+
+    <!-- Description -->
+    <p class="text-gray-400 text-sm mb-4">
+      ${issue.description}
+    </p>
+
+    <!-- Labels -->
+    <div class="flex gap-2 mb-4">
+      <span class="flex items-center gap-1 bg-red-100 text-red-400 text-xs font-semibold px-3 py-1 rounded-full">
+         ${issue.labels[0]}
+      </span>
+      <span class="flex items-center gap-1 bg-yellow-100 text-yellow-600 text-xs font-semibold px-3 py-1 rounded-full">
+        ${issue.labels[1]}
+      </span>
+    </div>
+
+    <!-- Divider -->
+    <hr class="border-gray-100 mb-3" />
+
+    <!-- Footer -->
+    <div class="text-gray-400 text-xs space-y-1">
+      <p><span class="text-gray-500 font-medium">#${issue.id}</span> &nbsp; by ${issue.assignee}</p>
+      <p>${issue.createdAt}</p>
+    </div>
+  </div>
+
+</div>
+        `;
+        allSection.appendChild(div);
+    }
+}
+
+// modal section
+
+
 
 loadAllData();
